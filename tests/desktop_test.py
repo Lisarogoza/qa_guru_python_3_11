ccea3b5af3b8f616dd74784fef329304b7f22b0f
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2023-03-15 17:24:26 +0300

    Added desktop test file

--- tests/desktop_test.py
+++ tests/desktop_test.py
@@ -1,7 +1,22 @@
+import pytest
 from conftest import browser
 from selene import have
 
 
-def test_github_desktop(open_desktop_browser):
+def test_desktop(browser_settings_desktop):
     browser.element('.HeaderMenu-link--sign-in').click()
     browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
+
+
+def test_skip_desktop(browser_settings_skip):
+    size = browser_settings_skip
+    if size == 'mobile':
+        pytest.skip('ЭТО ДЕСКТОПНЫЙ ТЕСТ')
+    browser.element('a[href="/login"]').click()
+    browser.element('[class="auth-form-body mt-3"]').should(have.text('Password'))
+
+
+@pytest.mark.parametrize('browser_settings', ['desktop'], indirect=True)
+def test_param_desktop(browser_settings):
+    browser.element('a[href="/login"]').click()
+    browser.element('[class="auth-form-header p-0"]').should(have.text('Sign in to GitHub'))
