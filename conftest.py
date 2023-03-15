3c3730f2db35f20254e9d26e16ae34b10f9474cd
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2023-03-15 17:24:05 +0300

    Added mobile test file

--- conftest.py
+++ conftest.py
@@ -2,15 +2,42 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture()
-def open_desktop_browser():
-    browser.config.window_width = 1600
-    browser.config.window_height = 1000
+@pytest.fixture(params=['desktop', 'mobile'])
+def browser_settings_skip(request):
+    if request.param == 'desktop':
+        browser.config.window_width = 1366
+        browser.config.window_height = 768
+    elif request.param == 'mobile':
+        browser.config.window_width = 375
+        browser.config.window_height = 812
+    browser.open('https://github.com')
+    return request.param
+
+
+@pytest.fixture(params=['desktop', 'mobile'])
+def browser_settings(request):
+    if request.param == 'desktop':
+        browser.config.window_width = 1366
+        browser.config.window_height = 768
+    elif request.param == 'mobile':
+        browser.config.window_width = 375
+        browser.config.window_height = 812
     browser.open('https://github.com')
 
 
-@pytest.fixture()
-def open_mobile_browser():
-    browser.config.window_width = 800
-    browser.config.window_height = 800
+@pytest.fixture
+def browser_settings_desktop():
+    browser.config.window_width = 1366
+    browser.config.window_height = 768
     browser.open('https://github.com')
+    yield
+    browser.quit()
+
+
+@pytest.fixture
+def browser_settings_mobile():
+    browser.config.window_width = 375
+    browser.config.window_height = 812
+    browser.open('https://github.com')
+    yield
+    browser.quit()
--- tests/mob_test.py
+++ tests/mob_test.py
@@ -1,8 +1,25 @@
+import pytest
 from conftest import browser
 from selene import have
 
 
-def test_github_mobile(open_mobile_browser):
+def test_mobile(browser_settings_mobile):
     browser.element('.Button-label').click()
     browser.element('.HeaderMenu-link--sign-in').click()
     browser.element('.auth-form-header').should(have.text('Sign in to GitHub'))
+
+
+def test_skip_mobile(browser_settings_skip):
+    size = browser_settings_skip
+    if size == 'desktop':
+        pytest.skip('ЭТО МОБИЛЬНЫЙ ТЕСТ')
+    browser.element('[class="Button-label"]').click()
+    browser.element('a[href="/login"]').click()
+    browser.element('[class="auth-form-body mt-3"]').should(have.text('Password'))
+
+
+@pytest.mark.parametrize('browser_settings', ['mobile'], indirect=True)
+def test_param_mobile(browser_settings):
+    browser.element('[class="Button-label"]').click()
+    browser.element('a[href="/login"]').click()
+    browser.element('[class="application-main "]').should(have.text('Password'))
